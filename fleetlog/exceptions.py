e52b1481126b4a18fce100b9389938b52d0bc25c
axel <boguiaxel06@gmail.com>
2026-09-10 03:24:28 +0000

    fix(fleet-log): sync exceptions.py with renamed classes

--- fleetlog/exceptions.py
+++ fleetlog/exceptions.py
@@ -1,18 +1,33 @@
-class VehiculeNonTrouveError(Exception):
-    """ Exception levée lorsque un vehicule n'existe pas dans le système """
-    pass
+class VehicleNotFoundError(Exception):
+    """Raised when the vehicle is not found"""
+    def __init__(self, registration):
+        message = f"registration {registration} not found"
+        super().__init__(message)
+        self.registration = registration
 
 
-class ImmatriculationInvalideError(Exception):
-    """ Exception levée lorsque une plaque d'immatriculation est vide ou mal formatée """
-    pass
+class InvalidRegistrationError(Exception):
+    """Raised when the vehicle registration is invalid"""
+    def __init__(self, registration):
+        message = f"registration {registration} invalid"
+        super().__init__(message)
+        self.registration = registration
 
 
-class KilometrageInvalideError(Exception):
-    """ Exception levée si le kilométrage saisi est négatif ou inférieur au kilométrage actuel du vehicule """
-    pass
+
+class InvalidMileageError(Exception):
+    """Raised if an entered mileage is negative or lower than the current mileage of the vehicle"""
+    def __init__(self, entered_mileage, current_mileage):
+        message = f"the entered mileage {entered_mileage} is negative or lower than current mileage {current_mileage}"
+        super().__init__(message)
+        self.entered_mileage = entered_mileage
+        self.current_mileage = current_mileage
 
 
-class CapaciteDepasseeError(Exception):
-    """ Exception levée si le poids de la cargaison depasse la capacité maximale du vehicule """
-    pass
+class CapacityExceededError(Exception):
+    """Raised if the weight of the cargo exceeds the maximum capacity of the vehicle"""
+    def __init__(self, cargo_weight, max_capacity):
+        message = f"the weight of the cargo {cargo_weight} is greater than the maximum capacity of the vehicle {max_capacity}"
+        super().__init__(message)
+        self.cargo_weight = cargo_weight
+        self.max_capacity = max_capacity
